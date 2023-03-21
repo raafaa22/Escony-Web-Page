92b76b5d73c8dc2c9d49c223d9985024d4f6966e
Rafa Serrano <rsa00016@red.ujaen.es>
2023-03-21 11:12:18 +0100

    AppConfig

---
+++ src/main/java/com/example/escony/AppConfig.java
@@ -0,0 +1,61 @@
+package com.example.escony;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+
+/** Configure App and JEE Security API
+ *
+ * @author jrbalsas
+ */
+/*
+    SELECT some IdentityStore implementations
+*/
+/* Soteria RI in memory IdentityStore (org.glassfish.soteria dependency needed in pom.xml */
+
+/* JEE Security Database IdentityStore implementation*/
+//@DatabaseIdentityStoreDefinition(
+//    dataSourceLookup = "java:global/jdbc/gestClub",
+//    callerQuery = "select password from authinfo where dni = ?",
+//    groupsQuery = "select rolname from authinfo where dni = ?",
+//    hashAlgorithm = Pbkdf2PasswordHash.class,
+//    //priorityExpression = "#{100}",
+//    hashAlgorithmParameters = {
+//        "Pbkdf2PasswordHash.Algorithm=PBKDF2WithHmacSHA512",
+//        "Pbkdf2PasswordHash.Iterations=3072",
+//        "Pbkdf2PasswordHash.SaltSizeBytes=64"
+//    }
+//)
+/*
+ * SELECT ONE HttpAuthenticationMchanismDefinition
+ */
+/* Use browser authentication dialog */
+//@BasicAuthenticationMechanismDefinition(
+//        realmName = "Club de Tenis"
+//)
+/* Estandard form validation with j_security_check action*/
+@FormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login.jsf",
+                errorPage = "/login.jsf?error",
+                useForwardToLogin = false
+        )
+)
+/* Custom form validation for programatic autentication (see ClubLoginController.authenticate())*/
+//@CustomFormAuthenticationMechanismDefinition(
+//        loginToContinue = @LoginToContinue(
+//                loginPage = "/customlogin.jsf",
+//                errorPage = "",
+//                useForwardToLogin = false
+//        )
+//)
+@ApplicationScoped
+@FacesConfig //Required for using JSF 2.3 features
+public class AppConfig {
+
+}
